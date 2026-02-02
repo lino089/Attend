08f2439f94918fe1ef07b15088171b8fa2cd5a93
lino089 <marvellino089@gmail.com>
2026-02-02 13:35:55 +0700

    perbaikan bugs

--- lib/presensiPage.dart
+++ lib/presensiPage.dart
@@ -11,79 +11,93 @@ class presensi extends StatefulWidget {
 class _presensi extends State<presensi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(70),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(alpha: 0.10),
-                blurRadius: 4,
-                offset: Offset(0, 4),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('siswa')
+          .orderBy('id')
+          .snapshots(),
+      builder: (context, snapshots) {
+        if (snapshots.hasError)
+          return const Scaffold(body: Center(child: Text('Error')));
+        if (snapshots.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        final docs = snapshots.data!.docs;
+
+        int totalHadir = docs.where((d) => d['status'] == 'H').length;
+        int totalSakit = docs.where((d) => d['status'] == 'S').length;
+        int totalIzin = docs.where((d) => d['status'] == 'I').length;
+        int totalAlpha = docs.where((d) => d['status'] == 'A').length;
+        int sudahAbsen = totalHadir + totalSakit + totalIzin + totalAlpha;
+
+        return Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(70),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.10),
+                    blurRadius: 4,
+                    offset: Offset(0, 4),
+                  ),
+                ],
               ),
-            ],
-          ),
-          child: AppBar(
-            backgroundColor: Colors.white,
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back_ios_new),
-              onPressed: () => Navigator.pop(context),
+              child: AppBar(
+                backgroundColor: Colors.white,
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                toolbarHeight: 70,
+                elevation: 0,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "XI RPL 2",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Pemrograman Perangkat bergerak",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey.withAlpha(155),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            toolbarHeight: 70,
-            elevation: 0,
-            title: Column(
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 30, right: 15, left: 15),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("XI RPL 2", style: TextStyle(fontWeight: FontWeight.bold)),
                 Text(
-                  "Pemrograman Perangkat bergerak",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey.withAlpha(155),
-                  ),
+                  'Progres Presensi : ${(sudahAbsen / docs.length * 100).toInt().clamp(0.0, 100.0)}%',
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 30, right: 15, left: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Progres Presensi'),
-            SizedBox(height: 5),
-            LinearProgressIndicator(
-              value: 0.0,
-              backgroundColor: Colors.grey,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-              minHeight: 10,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            SizedBox(height: 40),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('siswa')
-                    .orderBy('id')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError)
-                    return const Text('Error mengambil data');
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  final docs = snapshot.data!.docs;
-                  return ListView.builder(
+                SizedBox(height: 5),
+                LinearProgressIndicator(
+                  value: sudahAbsen / docs.length,
+                  backgroundColor: Colors.grey,
+                  valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+                  minHeight: 10,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                SizedBox(height: 40),
+                Expanded(
+                  child: ListView.builder(
                     itemCount: docs.length,
                     itemBuilder: (context, index) {
                       var data = docs[index].data() as Map<String, dynamic>;
-
                       return Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.only(bottom: 12.0),
                         child: card(
                           selectedStatus: false,
                           nama: data['nama'] ?? '-',
@@ -94,15 +108,100 @@ class _presensi extends State<presensi> {
                         ),
                       );
                     },
-                  );
-                },
-              ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+
+          bottomNavigationBar: Container(
+            height: 160,
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10,
+                  offset: Offset(0, -5),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    boxRekap(
+                      totalHadir.toString(),
+                      "Hadir",
+                      const Color(0xFFFFF8E1),
+                    ),
+                    boxRekap(
+                      totalSakit.toString(),
+                      "Sakit",
+                      const Color(0xFFFFE0B2),
+                    ),
+                    boxRekap(
+                      totalIzin.toString(),
+                      "Izin",
+                      const Color(0xFFE1F5FE),
+                    ),
+                    boxRekap(
+                      totalAlpha.toString(),
+                      "Alpha",
+                      const Color(0xFFFFCDD2),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: (){
+
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFBA68C8),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(12)),
+                    ),
+                    child: Text(
+                      "Kirim Presensi ($sudahAbsen)",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
+}
+
+Widget boxRekap(String jumlah, String label, Color warna) {
+  return Container(
+    width: 75,
+    padding: const EdgeInsets.symmetric(vertical: 8),
+    decoration: BoxDecoration(
+      color: warna,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Column(
+      children: [
+        Text(
+          jumlah,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 10),
+        ),
+        Text(label, style: const TextStyle(fontSize: 10)),
+      ],
+    ),
+  );
 }
 
 class card extends StatelessWidget {
@@ -232,7 +331,7 @@ class card extends StatelessWidget {
         height: 30,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          border: Border.all(color: isActive ? activeColor : Colors.black,),
+          border: Border.all(color: isActive ? activeColor : Colors.black),
           color: isActive ? activeColor : Colors.transparent,
         ),
         child: Center(
